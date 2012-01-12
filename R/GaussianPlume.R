453448c1b9e16e0ca46be11fe9e8bc23c6decde2
David Holstius <david.holstius@berkeley.edu>
2012-01-11 20:37:58 -0800

    Add man pages and update vignette

--- R/GaussianPlume.R
+++ R/GaussianPlume.R
@@ -21,14 +21,13 @@ GaussianPlume <- function(Q, H, u, sigma=PasquillGifford('D')) {
 			z <- receptors[,3]
 		}
 	    sg <- sigma(x)
-	    lateral <- exp(-(y ^ 2) / (2 * sg$y ^ 2))
+	    crosswind <- exp(-(y ^ 2) / (2 * sg$y ^ 2)) / (sg$y * sqrt(2*pi))
 	    v1 <- -(z - H) ^ 2 / (2 * sg$z ^ 2)
 	    v2 <- -(z + H) ^ 2 / (2 * sg$z ^ 2)
-	    vertical <- exp(v1) + exp(v2)
-	    K <- 1 / (2*pi * u * sg$y * sg$z)
-	    pred <- Q * K * lateral * vertical
-	    names(pred) <- row.names(receptors)
-	    return(pred)
+	    vertical <- (exp(v1) + exp(v2)) / (sg$z * sqrt(2*pi))
+	    concentrations <- Q / u * crosswind * vertical
+	    names(concentrations) <- row.names(receptors)
+	    return(concentrations)
  	}
     return(f)
 } 
